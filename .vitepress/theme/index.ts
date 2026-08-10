25465f98ff59ed9f72ade6c3950ab21dac9fbdf7
xbsheng <xxbsheng@gmail.com>
2026-08-10 23:10:01 +0800

    feat: add pageview tracking with Baidu Analytics on route change

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import { useRouter } from 'vitepress'
 import Giscus from './components/Giscus.vue'
 
-declare const __COMMIT_SHA__: string
-
 // 构建期由 vite define 注入，本地构建显示 dev
+declare const __COMMIT_SHA__: string
 console.log('[atguigu-note] commit:', __COMMIT_SHA__)
+
+// 百度统计 pv 统计
+declare const _hmt: any[]
+const router = useRouter()
+router.onAfterRouteChange = to => {
+  _hmt.push(['_trackPageview', to])
+}
 
 export default {
   extends: DefaultTheme,
