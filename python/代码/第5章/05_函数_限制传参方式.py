71250754db5d63688ebc9b886655a7029016eb0b
wangyalong <2261045584@qq.com>
2026-09-03 18:12:28 +0800

    第五章-

--- python/代码/第5章/05_函数_限制传参方式.py
+++ python/代码/第5章/05_函数_限制传参方式.py
@@ -1,7 +1,7 @@
 # 定义函数（使用/和*限制传参方式）
 def greet(name, /, gender, *, age, height):
     print(f'我叫{name}，性别{gender}，年龄是{age}，身高是{height}cm')
-# 位置参数 关键字参数
+# 位置参数 关键字参数 /必须在*号之前
 # 正确示例
 greet('张三', '男', age=18, height=172)
 greet('张三', gender='男', age=18, height=172)
